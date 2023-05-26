d4b53fcc190ccfcaac3c875925372e9c45a444b2
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-05-26 09:09:29 +0100

    Todays 8 kyu

---
+++ 8 kyu/jennys_secret_message.js
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/javascript
+
+function greet(name){
+
+    if(name === "Johnny") {
+      return "Hello, my love!";}
+    else   {return "Hello, " + name + "!";}
+  }
