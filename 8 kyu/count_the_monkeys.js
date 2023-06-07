037e899934a0439021cf46b03cc9fd9669d9dba4
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-07 09:12:08 +0100

    Forgot to add code for one kata

---
+++ 8 kyu/count_the_monkeys.js
@@ -0,0 +1,9 @@
+// link : https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+function monkeyCount(n) {
+    let result = [];
+    for (let i=1; i<=n; i++) {
+      result.push(i);
+    }
+    return result;
+  }
