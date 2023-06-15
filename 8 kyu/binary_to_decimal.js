07949a38444617b21edfe8227d66596b0c09c812
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-15 10:09:24 +0100

    Todays completed katas

---
+++ 8 kyu/binary_to_decimal.js
@@ -0,0 +1,5 @@
+// link : https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
+
+function binToDec(bin){
+    return parseInt(bin, 2);
+  }
---
+++ 8 kyu/whats_the_real_floor.js
@@ -0,0 +1,7 @@
+// link : https://www.codewars.com/kata/574b3b1599d8f897470018f6
+
+function getRealFloor(n) {
+  if (n<=0) {return n}
+  else if (n<13) {return n-1}
+  else {return n-2}
+}
