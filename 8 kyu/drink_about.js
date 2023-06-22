8920c67a1309bb10ae918d79e2b9222dd6c07258
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-22 09:03:23 +0100

    Completed 1 kata

---
+++ 8 kyu/drink_about.js
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/56170e844da7c6f647000063
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {return "drink toddy";}
+  else if (old <18) {return "drink coke";}
+  else if (old<21) {return "drink beer";}
+  else {return "drink whisky";}
+};
