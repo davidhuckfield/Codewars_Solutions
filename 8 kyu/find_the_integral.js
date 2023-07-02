a12777e517289f8f09de38e388ee4c590155f063
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-07-02 12:03:09 +0100

    Completed 2 more 8kyu katas

---
+++ 8 kyu/find_the_integral.js
@@ -0,0 +1,5 @@
+// link : https://www.codewars.com/kata/59811fd8a070625d4c000013
+
+function integrate(coefficient, exponent) {
+    return (coefficient/(exponent+1)).toString() + "x^" + (exponent+1).toString();
+  }
---
+++ 8 kyu/take_the_derivative.js
@@ -0,0 +1,5 @@
+// link : https://www.codewars.com/kata/5963c18ecb97be020b0000a2
+
+function derive(coefficient,exponent) {
+    return (coefficient*exponent).toString() + "x^" + (exponent-1).toString(); 
+  }
