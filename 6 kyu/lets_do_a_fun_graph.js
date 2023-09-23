bca15d9ac9654139fd9d8d4b7ae183cfffe680a2
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-09-23 18:25:46 +0100

    Completed 6 kyu kata to create a graph from an array

--- 6 kyu/lets_do_a_fun_graph.js
+++ 6 kyu/lets_do_a_fun_graph.js
@@ -3,12 +3,39 @@
 function graph(arr) {
     let outerArray=[];
     let maxNumber = Math.max(...arr);
-    for (let i=0; i<maxNumber;i++) {
+    for (let i=0; i<=maxNumber;i++) {
       outerArray.push([]);
     }
-    for (let i=outerArray.length-1; i>=0; i--) {
-      let numberLabel=outerArray.length-i;
-      outerArray[i].push("|" + numberLabel)
+    
+  for (let i=0; i<outerArray.length; i++) {
+    let currentXValue = outerArray.length-i-1;
+    for (let y=0; y<arr.length; y++) {
+      if (arr[y]===currentXValue) {
+        outerArray[i].push(" ____ ");
+      }
+      else if (arr[y]>currentXValue) {
+        outerArray[i].push("|    |");
+      }
+      else {
+        outerArray[i].push("......");
+      }
     }
-    console.table(outerArray);
   }
+
+  for (let i=outerArray.length-1; i>=0; i--) {
+    let numberLabel=outerArray.length-i-1;
+    if (i===0) {
+      outerArray[i].push(" ^ " + numberLabel);
+    } else {
+    outerArray[i].push(" | " + numberLabel)
+  }
+}
+
+    // console.table(outerArray);
+
+let joinedArray = outerArray.map(x=>x.join("")).join("\n");
+
+// console.log(joinedArray);
+return joinedArray;
+}
+  graph([10,5,3,1,4]);
