2b8d7c9cf73c739f1e62b96cd846613abf3f3188
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-09-28 16:37:25 +0100

    Reworked coin flip experiment code, laid out initial structure and plan

--- random_stuff/coin_flips.js
+++ random_stuff/coin_flips.js
@@ -1,93 +1,26 @@
-function coinFlips(flips) {
-    let heads = 0;
-    let tails = 0;
-    let flipsArray = [];
+function coinFlips(flips, run, initialBet) {
 
-    for (let i = 0; i < flips; i++) {
-        if (Math.random() < 0.5) {
-            heads++;
-            flipsArray.push("H");
-        } else {
-            tails++;
-            flipsArray.push("T");
-        }
-    }
-
+    //declare variables for number of heads or tails in a row, and the maximum run of heads or tails
     let headsrow = 0;
     let tailsrow = 0;
     let maxheadsrow = 0;
     let maxtailsrow = 0;
-    let initialBet = 5;
-    let betChoice = "";
-    let totalMoney = 5;
-    let minMoney = totalMoney;
-    let maxMoney = totalMoney;
+    
+    //declare variables for total money, max money reached, and min money reached
+    let totalMoney =0;
+    let maxMoney = 0;
+    let minMoney = 0;
+    
+    //declare current bet which may change, initialise it as initialBet input
+    //declare currentChoice (heads or tails / H or T)
+    let currentBet = initialBet;
+    let currentChoice = "";
 
-    for (let i = 0; i < flipsArray.length; i++) {
-        if (flipsArray[i] == "H") {
-            headsrow++;
-            if (headsrow > maxheadsrow) {
-                maxheadsrow = headsrow;
-            }
-            if (headsrow >= 3) {
-                betChoice = "T";
-                totalMoney -= initialBet;
-                if (totalMoney < minMoney) {
-                    minMoney = totalMoney;
-                }
-            }
-            if (betChoice === "H") {
-                totalMoney += initialBet*2;
-                if (totalMoney > maxMoney) {
-                    maxMoney = totalMoney;
-                }
-            }
-            else if (betChoice === "T") {
-                totalMoney -= initialBet;
-                if (totalMoney < minMoney) {
-                    minMoney = totalMoney;
-                }
-            }
-            tailsrow = 0;
-        } else {
-            tailsrow++;
-            if (tailsrow > maxtailsrow) {
-                maxtailsrow = tailsrow;
-            }
-            if (tailsrow >= 3) {
-                betChoice = "H";
-                totalMoney -= initialBet;
-                if (totalMoney < minMoney) {
-                    minMoney = totalMoney;
-                }
-            }
-            if (betChoice === "T") {
-                totalMoney += initialBet*2;
-                if (totalMoney > maxMoney) {
-                    maxMoney = totalMoney;
-                }
-            }
-            else if (betChoice === "H") {
-                totalMoney -= initialBet;
-                if (totalMoney < minMoney) {
-                    minMoney = totalMoney;
-                }
-            }
-            headsrow = 0;
+    //outer loop to perform the flips
+        for (let i=0; i<flips; i++) {
+
         }
-    }
-
-    
-
-
-    console.log(`Heads: ${heads}, Tails: ${tails}`);
-    console.log(`Longest run of heads: ${maxheadsrow}`);
-    console.log(`Longest run of tails: ${maxtailsrow}`);
-    console.log(`Total money: ${totalMoney}`);
-    console.log(`Minimum money: ${minMoney}`);
-    console.log(`Maximum money: ${maxMoney}`);
-    console.log(flipsArray)
-    
 }
 
+//call function with number of flips to perform
 coinFlips(10);
