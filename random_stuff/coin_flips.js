80df34baed6f32fd7df96b864c8a49d7570e25c0
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-09-28 16:41:32 +0100

    Further logic and steps added to coin flip code experiment

--- random_stuff/coin_flips.js
+++ random_stuff/coin_flips.js
@@ -19,6 +19,29 @@ function coinFlips(flips, run, initialBet) {
     //outer loop to perform the flips
         for (let i=0; i<flips; i++) {
 
+            //if random number is below 0.5, then heads, has been flipped
+            if (Math.random() < 0.5) {
+                //increase the number of heads in a row by 1
+                headsrow++;
+                //reset tails in a row to 0, as heads has been flipped
+                tailsrow = 0;
+                //if the current heads in a row is greater than the max heads in a row, then set max heads in a row to current heads in a row
+                if (headsrow > maxheadsrow) {
+                    maxheadsrow = headsrow;
+                }
+            }
+
+            //otherwise, tails has been flipped
+            else {
+                //increase the number of tails in a row by 1
+                tailsrow++;
+                //reset heads in a row to 0, as tails has been flipped
+                headsrow = 0;
+                //if the current tails in a row is greater than the max tails in a row, then set max tails in a row to current tails in a row
+                if (tailsrow > maxtailsrow) {
+                    maxtailsrow = tailsrow;
+                }
+            }
         }
 }
 
