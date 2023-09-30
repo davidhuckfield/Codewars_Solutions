d9c11651a4a4a263a245d580747811b1fbb57bb1
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-09-30 15:07:04 +0100

    Completed 8 kyu kata to determine if one input is a factor of another

---
+++ 8 kyu/check_for_factor.js
@@ -0,0 +1,6 @@
+// link : https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+function checkForFactor (base, factor) {
+    if (base % factor === 0) {return true;}
+    else {return false;}
+  }
