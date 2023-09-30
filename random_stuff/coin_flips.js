f9493fc4d067d8a1b17cf90e39ea0d92268faa0c
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-09-30 15:18:21 +0100

    Further work on coin flip simulator

--- random_stuff/coin_flips.js
+++ random_stuff/coin_flips.js
@@ -1,6 +1,8 @@
 function coinFlips(flips, run, initialBet) {
 
     //declare variables for number of heads or tails in a row, and the maximum run of heads or tails
+    let totaltails = 0;
+    let totalheads = 0;
     let headsrow = 0;
     let tailsrow = 0;
     let maxheadsrow = 0;
@@ -21,6 +23,8 @@ function coinFlips(flips, run, initialBet) {
 
             //if random number is below 0.5, then heads has been flipped
             if (Math.random() < 0.5) {
+                //increase the count of heads
+                totalheads++;
                 //increase the number of heads in a row by 1
                 headsrow++;
                 //reset tails in a row to 0, as heads has been flipped
@@ -30,11 +34,34 @@ function coinFlips(flips, run, initialBet) {
                     maxheadsrow = headsrow;
                 }
                 //resolve current bet 
+                if (currentChoice == "H") {
+                    totalMoney += currentBet*2;
+                    //update max money if necessary
+                    if (totalMoney > maxMoney) {
+                        maxMoney = totalMoney;
+                    }
+                }
+                //decide whether or not to place new bet based on run
+                if (headsrow >= run) {
+                    currentBet = initialBet;
+                    currentChoice = "T";
+                    totalMoney -= currentBet;
+                    //update min money if necessary
+                    if (totalMoney < minMoney) {
+                        minMoney = totalMoney;
+                    }
+                }
+                else {
+                    currentBet = 0;
+                    currentChoice = "";    
+                }
                 
             }
 
             //otherwise, tails has been flipped
             else {
+                //increase the count of tails
+                totaltails++;
                 //increase the number of tails in a row by 1
                 tailsrow++;
                 //reset heads in a row to 0, as tails has been flipped
@@ -43,9 +70,39 @@ function coinFlips(flips, run, initialBet) {
                 if (tailsrow > maxtailsrow) {
                     maxtailsrow = tailsrow;
                 }
+                //resolve current bet 
+                if (currentChoice == "T") {
+                    totalMoney += currentBet*2;
+                    //update max money if necessary
+                    if (totalMoney > maxMoney) {
+                        maxMoney = totalMoney;
+                    }
+                }
+                //decide whether or not to place new bet based on run
+                if (tailsrow >= run) {
+                    currentBet = initialBet;
+                    currentChoice = "H";
+                    totalMoney -= currentBet;
+                    //update min money if necessary
+                    if (totalMoney < minMoney) {
+                        minMoney = totalMoney;
+                    }
+                }
+                else {
+                    currentBet = 0;
+                    currentChoice = "";    
+                }
             }
         }
+
+        console.log("Max heads in a row: " + maxheadsrow);
+        console.log("Max tails in a row: " + maxtailsrow);
+        console.log("Total money: " + totalMoney);
+        console.log("Total heads: " + totalheads);
+        console.log("Total tails: " + totaltails);
+        console.log("Max money: " + maxMoney);
+        console.log("Min money: " + minMoney);
 }
 
 //call function with number of flips to perform, run of heads or tails to bet at, and initial bet
-coinFlips(10, 3, 5);
+coinFlips(100, 3, 5);
