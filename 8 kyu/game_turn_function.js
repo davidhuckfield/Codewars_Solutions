2bc0b493197e5bc139ce91ac457438b961377416
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-07 17:16:52 +0100

    Completed 8 kyu kata with game move subfunctions

---
+++ 8 kyu/game_turn_function.js
@@ -0,0 +1,10 @@
+// link : https://www.codewars.com/kata/56019d3b2c39ccde76000086
+
+function doTurn () {
+    rollDice();
+    move();
+    combat();
+    getCoins();
+    buyHealth();
+    printStatus();
+  }
