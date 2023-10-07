73ca65af2023b75e930c67fb031a2e745acf260a
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-07 18:25:43 +0100

    Completed 6 kyu kata solution for partial array reversing - working when tested in console, both solutions timing out on Codewars - possible server issues, try again later

--- 6 kyu/selective_array_reversing.js
+++ 6 kyu/selective_array_reversing.js
@@ -1,2 +1,38 @@
 // link : https://www.codewars.com/kata/58f6000bc0ec6451960000fd
 
+function selReverse(array, length) {
+    let returnArray = [];
+    let currentPosition = 0;
+    let currentSlice;
+    
+    while (currentPosition<array.length) {
+      
+    if (currentPosition + length > array.length-1) {
+      currentSlice = array.slice(currentPosition, array.length)
+    } 
+    else {
+      currentSlice = array.slice(currentPosition, currentPosition+length)
+    }
+      
+    currentSlice.reverse();
+    returnArray.push(...currentSlice);
+    currentPosition+=length;
+      }
+    return returnArray;
+  }
+
+  //other solution using for loop, accounts for ending portions because giving an end coordinate to slice which is greater than the array end will just return the rest of the array
+
+  function selReverse(array, length) {
+    const returnArray = [];
+    for (let i = 0; i < array.length; i += length) {
+      const currentSlice = array.slice(i, i + length);
+      currentSlice.reverse();
+      returnArray.push(...currentSlice);
+    }
+    return returnArray;
+  }
+
+
+  console.log(selReverse([2,4,6,8,10,12,14,16], 3))
+
