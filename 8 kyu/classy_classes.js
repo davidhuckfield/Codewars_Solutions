203e6440105f39109a4e5e26d97573135ccac474
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-10 14:49:19 +0100

    Completed 8 kyu kata working with classes and constructors

---
+++ 8 kyu/classy_classes.js
@@ -0,0 +1,18 @@
+// link : https://www.codewars.com/kata/55a144eff5124e546400005a
+
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    getInfo () {
+      return this.name + "s age is " + this.age;
+    }
+    
+    get info() {
+      return this.getInfo();
+    }
+    
+    
+  }
