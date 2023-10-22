66f36018b3262c3eec66389ddfaf491a0a505146
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-22 15:15:09 +0100

    Completed 8 kyu kata working with classes and random attribute generation

---
+++ 8 kyu/color_ghost.js
@@ -0,0 +1,16 @@
+// link : https://www.codewars.com/kata/53f1015fa9fe02cbda00111a
+
+var Ghost = function() {
+    let randomNumber = Math.random();
+    let color;
+    if (randomNumber <= 0.25) {
+      color = "white";
+    } else if (randomNumber <= 0.5) {
+      color = "yellow";
+    } else if (randomNumber <= 0.75) {
+      color = "purple";
+    } else {
+      color = "red";
+    }
+    this.color = color;
+  };
