dd114e0a6fb7be5afbc7727fb49819be47936df6
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-29 14:16:06 +0000

    Completed 8 kyu kata to find nearest square number to input

---
+++ 8 kyu/find_nearest_square_number.js
@@ -0,0 +1,28 @@
+// link : https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+
+function nearestSq(n){
+    if (Math.sqrt(n)%1 === 0) {return n;}
+    let nearestSqbelow=0;
+    let nearestSqabove=0;
+    let nBelow=n;
+    let nAbove=n;
+    while (nearestSqbelow===0) {
+      nBelow--;
+      if (Math.sqrt(nBelow)%1===0) {
+        nearestSqbelow = nBelow;
+      }
+    }
+     while (nearestSqabove===0) {
+      nAbove++;
+      if (Math.sqrt(nAbove)%1===0) {
+        nearestSqabove = nAbove;
+      }
+    }
+    let diffBelow = n-nearestSqbelow;
+    let diffAbove = nearestSqabove-n;
+    let nearestAbsolute = diffBelow<diffAbove ? nearestSqbelow : nearestSqabove;
+
+    return nearestAbsolute;
+  }
+
+  nearestSq(10);
