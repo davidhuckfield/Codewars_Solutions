101b465fcdd572ff04b4ac3708a19606af659219
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-31 14:17:27 +0000

    Completed 8 kyu kata to call different functions based on boolean input

---
+++ 8 kyu/the_if_function.js
@@ -0,0 +1,10 @@
+// link : https://www.codewars.com/kata/54147087d5c2ebe4f1000805
+
+function _if(bool, func1, func2) {
+    if (bool) {
+      func1();
+    }
+    else {
+      func2();
+    }
+  }
