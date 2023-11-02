fb5349250d5fce4b231328937f7018765edcdf62
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-02 16:30:56 +0000

    Completed 8 kyu kata to debug function that returns object

---
+++ 8 kyu/unexpected_parsing.js
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/54fdaa4a50f167b5c000005f
+
+function getStatus(isBusy) {
+    var msg = (isBusy ? "busy" : "available");
+    return {
+      status: msg
+    }
+  }
