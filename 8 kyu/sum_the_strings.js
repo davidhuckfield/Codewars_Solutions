47c29c8ed92079021a550a59c2ab7d63693bb579
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-23 18:20:34 +0000

    Completed 8 kyu kata to add together two number strings and return as strings, default to 0 if empty

---
+++ 8 kyu/sum_the_strings.js
@@ -0,0 +1,6 @@
+// link : https://www.codewars.com/kata/5966e33c4e686b508700002d
+
+function sumStr(a,b) {
+    let result = (parseInt(a) || 0) + (parseInt(b) || 0)
+    return result.toString();
+  }
