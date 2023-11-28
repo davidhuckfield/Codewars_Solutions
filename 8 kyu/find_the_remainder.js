e29b09616a1664b2ac7b58b486c91c5b66125bdf
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-28 17:00:10 +0000

    Completed 8 kyu kata to return remainder of dividing larger input by smaller

---
+++ 8 kyu/find_the_remainder.js
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/524f5125ad9c12894e00003f
+
+function remainder(n, m){
+    let max = Math.max(n, m);
+    let min = Math.min(n, m);
+    return max%min;
+    
+  }
