77161acf8f8599c670664407d62e9193aa526172
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-12-01 18:51:00 +0000

    Completed 8 kyu kata dealing with classes to create a template hero for a game

---
+++ 8 kyu/terminal_game_1.js
@@ -0,0 +1,11 @@
+// link : https://www.codewars.com/kata/55e8aba23d399a59500000ce/train/javascript
+
+class Hero {
+    constructor (name, position, health, damage, experience) {
+      this.name = name || "Hero";
+      this.position = "00";
+      this.health = 100;
+      this.damage = 5;
+      this.experience = 0;
+    }
+  }
