7d446c28be459ef8c1f1f597315a1cbd1ab255b0
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-12-04 21:39:17 +0000

    Completed 8 kyu kata using if/else and multiplication

---
+++ 8 kyu/if_else_ternary_operator.js
@@ -0,0 +1,7 @@
+// link : https://www.codewars.com/kata/57202aefe8d6c514300001fd
+
+function saleHotdogs(n){
+    if (n<5) {return n*100;}
+    else if (n>=5 && n<10) {return n*95;}
+    else {return n*90;}
+  }
