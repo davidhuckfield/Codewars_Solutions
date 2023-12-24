a899fafeb2aefac45267a1de205942314e72c4d8
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-12-24 21:12:44 +0000

    Completed 8 kyu kata to check if a string contains a certain substring but non-case sensitive

---
+++ 8 kyu/do_you_speak_english.js
@@ -0,0 +1,5 @@
+// link : https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058
+
+function spEng(sentence){
+    return sentence.toLowerCase().includes("english");
+    }
---
+++ 8 kyu/hello_name_or_world.js
@@ -0,0 +1,2 @@
+// link : https://www.codewars.com/kata/57e3f79c9cb119374600046b
+
