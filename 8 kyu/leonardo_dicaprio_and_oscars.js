3fa1facdbf420c3e6fc2d361b7badb986a31e281
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-01-06 11:39:36 +0000

    COmpleted 8 kyu kata to return different strings based on value of input

---
+++ 8 kyu/leonardo_dicaprio_and_oscars.js
@@ -0,0 +1,15 @@
+// link : https://codewars.com/kata/56d49587df52101de70011e4
+
+function leo(oscar){
+    switch (true) {
+    case oscar === 88: return "Leo finally won the oscar! Leo is happy";
+    break;
+    case oscar === 86: return "Not even for Wolf of wallstreet?!";
+    break;
+    case oscar < 88: return "When will you give Leo an Oscar?";
+    break;
+    case oscar > 88: return "Leo got one already!";
+    break;
+        
+  }
+  } 
