063828bc56eea65af2ff15c02898a5056576cefe
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-01-13 16:02:45 +0000

    Added simpler solution to previous 7 kyu kata

--- 7 kyu/even_numbers_in_an_array.js
+++ 7 kyu/even_numbers_in_an_array.js
@@ -16,3 +16,8 @@ function evenNumbers(array, number) {
   }
 
   console.log(evenNumbers([1,2,3,4,5,6,7,8,9], 3));
+
+
+  //simpler solution
+
+  //const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
