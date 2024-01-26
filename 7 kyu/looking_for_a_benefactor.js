a9dbb1b8255e32c87b383ab3515c39c3fcd955d2
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-01-26 13:19:26 +0000

    Completed 7 kyu kata to calculate the next number that will make the average of an array of numbers a target value

---
+++ 7 kyu/looking_for_a_benefactor.js
@@ -0,0 +1,23 @@
+// link : https://www.codewars.com/kata/569b5cec755dd3534d00000f
+
+//find the value of the next donation that would make the average 30
+//if total divided by number of donations = 30, total + x = number of donations x 30
+
+function newAvg(arr, newavg) {
+    //eventual number of donations will be the existing ones, plus the next one
+    let numberOfDonations = arr.length+1;
+    //the amount he wants to reach is the number of donations x 30, thereby making 30 the avg. donation
+    let totalTargetAmount = numberOfDonations * newavg;
+    //the existing total of donation money is the sum of the array numbers
+    let existingTotal = arr.reduce((acc, curr) => acc + curr, 0);
+    //his next target donation is the target amount minus the current amount
+    let nextTargetDonation = totalTargetAmount - existingTotal;
+    //handle the potential error of the next target donation being 0 or negative
+  if (nextTargetDonation<=0) {
+    throw new Error("Expected New Average is too low");
+  }
+  //round up to the nearest integer
+    return Math.ceil(nextTargetDonation);
+}
+
+console.log(newAvg([14,30,5,7,9,11,15]));
