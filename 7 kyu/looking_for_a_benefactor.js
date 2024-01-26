7b0439923bc4b2bff491e91d655f216aae7e86f7
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-01-26 13:20:18 +0000

    Corrected comments on last kata, to input value rather than fixed value of 30

--- 7 kyu/looking_for_a_benefactor.js
+++ 7 kyu/looking_for_a_benefactor.js
@@ -1,12 +1,12 @@
 // link : https://www.codewars.com/kata/569b5cec755dd3534d00000f
 
-//find the value of the next donation that would make the average 30
+//find the value of the next donation that would make the average 30 (or specified input)
 //if total divided by number of donations = 30, total + x = number of donations x 30
 
 function newAvg(arr, newavg) {
     //eventual number of donations will be the existing ones, plus the next one
     let numberOfDonations = arr.length+1;
-    //the amount he wants to reach is the number of donations x 30, thereby making 30 the avg. donation
+    //the amount he wants to reach is the number of donations x target average, thereby making the target average the avg. donation
     let totalTargetAmount = numberOfDonations * newavg;
     //the existing total of donation money is the sum of the array numbers
     let existingTotal = arr.reduce((acc, curr) => acc + curr, 0);
