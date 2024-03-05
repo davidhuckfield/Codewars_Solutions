1dc5f89e3bd5dc96b6a032495299bf360c3d2472
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-03-05 18:49:07 +0000

    Completed 8 kyu kata to return various mathematical operations

---
+++ 8 kyu/fundamentals_return.js
@@ -0,0 +1,25 @@
+// link : https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+function add(a,b){
+    return a+b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function mod(a,b){
+    return a % b;
+}
+   
+function exponent(a,b){
+    return Math.pow(a,b);
+}
+    
+function subt(a,b){
+    return a-b;
+}
