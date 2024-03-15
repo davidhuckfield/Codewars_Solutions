f560429b327dbc627dd20e81a71f8247adb84767
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-03-15 13:08:25 +0000

    Completed 8 kyu kata previously started, to check for empty or undefined input then return hello input or world

--- 8 kyu/hello_name_or_world.js
+++ 8 kyu/hello_name_or_world.js
@@ -1,2 +1,14 @@
 // link : https://www.codewars.com/kata/57e3f79c9cb119374600046b
 
+function hello(name) {
+    let finalName;
+    if (!name || name==="") {
+      finalName = "World";
+    }
+    else {
+      finalName = name
+    }
+  
+    let formattedName = finalName.charAt(0).toUpperCase() + finalName.substring(1).toLowerCase();
+    return `Hello, ${formattedName}!`
+  }
