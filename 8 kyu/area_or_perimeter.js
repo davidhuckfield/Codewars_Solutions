85fd85b03efb6a010ac4b21c2489cc48215a9677
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-04-10 17:35:17 +0100

    Added alternative version for previously completed kata

--- 8 kyu/area_or_perimeter.js
+++ 8 kyu/area_or_perimeter.js
@@ -4,3 +4,10 @@ const areaOrPerimeter = function(l , w) {
     if (l === w) {return l*w;}
     else {return l*2 + w*2;}
   };
+
+  /*
+  const areaOrPerimeter = function(l , w) {
+    return l === w ? l*w : return l*2 + w*2;
+  };
+
+  */
