a050a9900a234f255e28defd375284e06d10fada
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-04-26 13:42:27 +0100

    Completed 7 kyu kata to return a copy of the input array, not modified or changed pointer

---
+++ 7 kyu/making_copies.js
@@ -0,0 +1,14 @@
+// link : https://www.codewars.com/kata/53d2697b7152a5e13d000b82
+
+function copyList(l){
+    let copy = [...l];
+    return copy;
+  }
+
+  // OR 
+
+  /*
+  function copyList(l){
+    return l.slice();
+  }
+  */
