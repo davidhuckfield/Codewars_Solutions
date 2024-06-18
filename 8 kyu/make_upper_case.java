db5bbcb7825daba681b670186dd8bc67b15d10e5
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-06-18 19:13:22 +0100

    Completed 8 kyu Java kata to convert string to uppercase

---
+++ 8 kyu/make_upper_case.java
@@ -0,0 +1,7 @@
+// link : https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+
+class Upper{
+  public static String MakeUpperCase(String str){
+      return str.toUpperCase();
+  }
+}
