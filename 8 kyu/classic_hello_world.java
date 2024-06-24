77804c4ee73b5c8d984bb32030315fed1999b4fe
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-06-24 20:24:20 +0100

    completed 8 kyu Java Hello World kata

---
+++ 8 kyu/classic_hello_world.java
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/57036f007fd72e3b77000023/train/java
+
+public class Solution {
+  public static void main(String[] args) {
+    
+    System.out.println("Hello World!\n");
+  }
+}
