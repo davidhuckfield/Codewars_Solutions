36d2e2875c0f333adce379b918189b3f6a8027c4
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-06-25 18:22:58 +0100

    Completed 8 kyu java kata on conditional returns

---
+++ 8 kyu/HelpAlex.java
@@ -0,0 +1,11 @@
+// link : https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/java
+
+public class HelpAlex{
+    public static String hoopCount(int n){
+     if (n >= 10) {
+       return "Great, now move on to tricks";
+     } else {
+       return "Keep at it until you get it";
+     }
+    }
+  }
