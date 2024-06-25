cddfa6bb1c3818bde2dc56fe14b8b08f465627f5
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-06-25 18:25:21 +0100

    Changed java file names to match classes

--- 8 kyu/classic_hello_world.java
+++ 8 kyu/classic_hello_world.java
@@ -1,6 +1,6 @@
 // link : https://www.codewars.com/kata/57036f007fd72e3b77000023/train/java
 
-public class Solution {
+public class classic_hello_world {
   public static void main(String[] args) {
     
     System.out.println("Hello World!\n");
