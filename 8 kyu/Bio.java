2903d996debd690fd4c26602b41e919a2caeb3c1
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-07-06 20:29:49 +0100

    Completed 8 kyu Java kata to replace characters in input string

---
+++ 8 kyu/Bio.java
@@ -0,0 +1,8 @@
+// link : https://www.codewars.com/kata/5556282156230d0e5e000089/train/java
+
+public class Bio {
+    public String dnaToRna(String dna) {
+        String rna = dna.replace("T", "U");
+      return rna;
+    } 
+}
