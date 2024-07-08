9bf3686d0500f3b323e4a9b98099b0c87c1d3cd8
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-07-08 18:08:10 +0100

    Completed 8 kyu Java kata to split strings and return specified characters

---
+++ 8 kyu/CharProblem.java
@@ -0,0 +1,11 @@
+// link : https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java
+
+public class CharProblem {
+    public static int howOld(final String herOld) {
+  
+      String[] letters = herOld.split("");
+      int age = Integer.parseInt(letters[0]);
+      return age;
+    
+    }
+  }
