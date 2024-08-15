f5511535d7fce7157ae6d2a46b3e4756db802def
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-08-15 14:16:19 +0100

    Completed 8 kyu Java kata to return even or odd for integer input

---
+++ 8 kyu/EvenOrOdd.java
@@ -0,0 +1,7 @@
+// link : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+
+public class Kata {
+    public static String evenOrOdd(int number) {
+        return number%2 == 0 ? "Even" : "Odd";
+    }
+}
