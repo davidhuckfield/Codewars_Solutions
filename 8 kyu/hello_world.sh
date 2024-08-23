e71c0f2344e3c5ab9ae499b4df39d7b52ea0013b
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-08-23 09:08:59 +0100

    Completed 8 kyu hello world kata in Shell

---
+++ 8 kyu/hello_world.sh
@@ -0,0 +1,12 @@
+# link : https://www.codewars.com/kata/57036f007fd72e3b77000023/train/shell
+
+#!/bin/bash
+function Solution
+{
+    function main
+    {
+        printf "Hello World!\n"
+    }
+    main
+}
+Solution
