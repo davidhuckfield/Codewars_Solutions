a28e7c0f542e120240452d3b1eaeab87649009dd
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-08-27 18:29:31 +0100

    Completed 7 kyu JS kata to return last of multiple inputs

---
+++ 7 kyu/last_input.js
@@ -0,0 +1,11 @@
+// link : https://www.codewars.com/kata/541629460b198da04e000bb9/train/javascript
+
+function last(list){
+  list = arguments.length === 1 ? arguments[0] : arguments;
+  
+  if (typeof list === "string" || Array.isArray(list)) {
+    return list[list.length-1];
+  }
+  
+  return arguments[arguments.length-1];
+}
