0f128ccfaffd02cee4ea6d5c9266596eb26326b4
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-09-02 13:56:37 +0100

    Completed 7 kyu JS kata on travel of animal vs moving finish line

---
+++ 7 kyu/snail.js
@@ -0,0 +1,15 @@
+// link : https://www.codewars.com/kata/66cdc6ab9e7a9f009e0ca8f6/train/javascript
+
+function canSnailReachEnd(length, speed, lengthIncreases) {
+    let snailPosition = 0;
+    let rubberBandEnd = length;
+    
+    for (let i=0; i<525600; i++) {
+      snailPosition += speed;
+      rubberBandEnd += lengthIncreases;
+      if (snailPosition >= rubberBandEnd) {
+        return true;
+      }
+    }
+  return false;
+}
