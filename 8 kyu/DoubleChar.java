95fa167ccf4a8a5d85bd54b5cacec7102749a211
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-09-04 21:59:33 +0100

    Fixed issues with Java files not having same name as class

--- 8 kyu/DoubleChar.java
+++ 8 kyu/DoubleChar.java
@@ -1,6 +1,8 @@
 // link : https://www.codewars.com/kata/56b1f01c247c01db92000076/train/java
 
-public class Solution{
+//original class name changed to match filename
+//public class Solution{
+public class DoubleChar{
   public static String doubleChar(String s){
     String doubleString = "";
     for (int i=0; i<s.length(); i++) {
--- 8 kyu/EvenOrOdd.java
+++ 8 kyu/EvenOrOdd.java
@@ -1,6 +1,8 @@
 // link : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
 
-public class Kata {
+//original class name changed to match filename
+//public class Kata {
+public class EvenOrOdd {
     public static String evenOrOdd(int number) {
         return number%2 == 0 ? "Even" : "Odd";
     }
