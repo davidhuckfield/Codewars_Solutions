09b39456821143296d831db0310145b7940dbe0d
Dave <99835156+davidhuckfield@users.noreply.github.com>
2024-09-23 14:30:25 +0100

    Completed 7 kyu JS kata on interlocking binary pairs

---
+++ 7 kyu/interlocking_binary_pairs.js
@@ -0,0 +1,16 @@
+// link : https://www.codewars.com/kata/628e3ee2e1daf90030239e8a/train/javascript
+
+function interlockable(a, b) {
+  let aBinary = a.toString(2).split("").reverse().join("");
+  let bBinary = b.toString(2).split("").reverse().join("");
+  let interlocking = true;
+  
+  let shortestLength = Math.min(aBinary.length, bBinary.length);
+  
+  for (let i=0; i<shortestLength; i++) {
+    if (parseInt(aBinary[i]) + parseInt(bBinary[i]) >1) {
+      interlocking = false;
+    }
+  }
+  return interlocking;
+}
