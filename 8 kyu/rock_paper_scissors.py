a9cea44dac221649374f9056f3b41759feb8e3ed
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-09-30 18:04:33 +0100

    Completed 8 kyu Rock Paper Scissors kata in Python

---
+++ 8 kyu/rock_paper_scissors.py
@@ -0,0 +1,13 @@
+# link : https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/python
+
+def rps(p1, p2):
+    if (p1 == p2):
+        return "Draw!"
+    elif (
+        (p1 == "scissors" and p2 == "paper") or
+        (p1 == "paper" and p2 == "rock") or
+        (p1 == "rock" and p2 == "scissors")
+    ):
+        return "Player 1 won!"
+    else:
+        return "Player 2 won!"
