2bc0f08f44f63531b9f3ba50f3bfff372902b282
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-10-14 11:32:14 +0100

    Added additional solution to previously completed 7 kyu Python kata

--- 7 kyu/find_calculation_type.py
+++ 7 kyu/find_calculation_type.py
@@ -11,3 +11,11 @@ def calc_type(a, b, res):
         return "division"
     else:
         return "Solution not found"
+    
+    # alternate solution
+
+def calc_type2(a, b, res):
+    # creates a dictionary of key-value pairs where each possible formula is calculated and the result is the key, and the return value is the value
+    # uses res as the key to look up, which will match one of the calculated numbers
+    # eg a is 1, b is 2, res is 3 - the dictionary will have the keys 3, -1, 2, 0.5.
+    return {a + b: "addition", a - b: "subtraction", a * b: "multiplication", a / b: "division"}[res]
