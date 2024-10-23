b25cd1cebe85d8abd528b2e152956352ae434e81
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-10-23 19:14:53 +0100

    Further changes to chinese zodiac kata, still not working

--- 7 kyu/chinese_zodiac.py
+++ 7 kyu/chinese_zodiac.py
@@ -1,32 +1,42 @@
 # link : https://www.codewars.com/kata/57a73e697cb1f31dd70000d2/train/python
 
-from preloaded import animals, elements
+# from preloaded import animals, elements
+
+animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
+elements = ["Wood", "Fire", "Earth", "Metal", "Water"]
 
 def chinese_zodiac(year):
-    print(year) #1965
-    print(animals)
-    print(elements)
-    ref_year = 1998
-    ref_animal = "Tiger"
-    ref_element = "Earth"
+    print(year)
+    # print(animals)
+    # print(elements)
+    ref_year = 1984
+    ref_animal = "Rat"
+    ref_element = "Wood"
     
-    animal_cycle_length = len(animals) #12
-    element_cycle_length = len(elements) #5
+    animal_cycle_length = len(animals)
+    element_cycle_length = len(elements)
     
-    ref_index_animal = animals.index(ref_animal) #2
-    ref_index_element = elements.index(ref_element) #2
+    ref_index_animal = animals.index(ref_animal)
+    ref_index_element = elements.index(ref_element)
     
-    year_difference = year - ref_year #-33
+    year_difference = year - ref_year
     
-    animal_index = (ref_index_animal + year_difference) % animal_cycle_length #-7
-    animal_index = animal_index if animal_index >=0 else animal_index + animal_cycle_length #5
-    target_animal = animals[animal_index] #Snake
+    animal_index = (ref_index_animal + year_difference) % animal_cycle_length
+    animal_index = animal_index if animal_index >=0 else animal_index + animal_cycle_length
+    target_animal = animals[animal_index]
     
-    element_index = (ref_index_element + year_difference) % element_cycle_length #-1
-    element_index = element_index if element_index >=0 else element_index + element_cycle_length #4
-    target_element = elements[element_index] #Water
+    element_index = (ref_index_element + year_difference) % element_cycle_length
+    element_index = element_index if element_index >=0 else element_index + element_cycle_length
+    target_element = elements[element_index]
     
     return_text = target_element + " " + target_animal
     return return_text
+
+print("1965 should be Wood Snake, it's:")
+print(chinese_zodiac(1965))
+print("1938 should be Earth Tiger, it's:")
+print(chinese_zodiac(1938))
+print("1965 should be Earth Tiger, it's:")
+print(chinese_zodiac(1998))
 
     
