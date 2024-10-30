e9a1ab5bd8087e0b4cec71b2048b853f2defff45
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-10-30 18:58:14 +0000

    Added comments

--- 7 kyu/fix_string_case.py
+++ 7 kyu/fix_string_case.py
@@ -1,6 +1,7 @@
 # link : https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/python
 
 def solve(s):
+    #use for to create total based on if condition
     lower_count = sum(1 for char in s if char.islower())
     upper_count = sum(1 for char in s if char.isupper())
     if upper_count > lower_count:
