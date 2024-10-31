2c455d337f6178b1a4a81ce43fb93dfaa175b2d0
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-10-31 17:21:33 +0000

    7 kyu Python kata for cypher in progress

---
+++ 7 kyu/mew_cipher.py
@@ -0,0 +1,26 @@
+# link : https://www.codewars.com/kata/671bd5419ea261fbb8d0a0ca/train/python
+
+import math
+
+def decipher(code):
+    print(f"code is {code}")
+    return_string = ""
+    for i in range(len(code[0])):
+        print(f"*** outer loop at index {i} ***")
+        print("          ")
+        sum = 0
+        for y in range(len(code)):
+            print(f"i index is {i} and y index is {y}")
+            print(f"character is {code[y][i]}")
+            print(f"code is {ord(code[y][i])-96}")
+            sum+=max(ord(code[y][i])-96, 0)
+            print(f"sum is now {sum}")
+        average = math.floor(sum/len(code))
+        print(f"average is {average}")
+        character = chr(average)
+        print(f"avg character is {character}")
+        return_string+=character
+        print(f"return_string is now {return_string}")
+        print("          ")
+        print(f"*** outer loop iterated ***")
+    return return_string
