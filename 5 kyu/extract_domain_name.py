55b72d961d994e773647a539bb0e57c09d638342
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-11-02 18:28:53 +0000

    Completed 5 kyu Python kata to extract the domain name from a URL

---
+++ 5 kyu/extract_domain_name.py
@@ -0,0 +1,24 @@
+# link : https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python
+
+def domain_name(url):
+
+    start_pos = 0
+    https_pos = url.find("https://")
+    http_pos = url.find("http://")
+    www_pos = url.find("www.")
+    http_www_pos = url.find("http://www.")
+    
+    if https_pos != -1:
+        start_pos = https_pos+8
+    elif http_www_pos != -1:
+        start_pos = http_www_pos+11
+    elif http_pos != -1:
+        start_pos = http_pos+7
+    elif www_pos != -1:
+        start_pos = www_pos+4
+        
+    trunc_url = url[start_pos:]
+    dot_pos = trunc_url.find(".")
+
+    return trunc_url[0:dot_pos]
+    
