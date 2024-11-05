b8500c3962461d1b268b6333b4dab6e8db40f6d2
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-11-05 15:42:26 +0000

    Completed 8 kyu Python kata to check if substrings in input string

---
+++ 8 kyu/strange_trip.py
@@ -0,0 +1,7 @@
+# link : https://www.codewars.com/kata/55ccdf1512938ce3ac000056/train/python
+
+def is_loch_ness_monster(string):
+    if "tree fiddy" in string or "3.50" in string or "three fifty" in string:
+        return True
+    else:
+        return False
