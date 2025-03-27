39bd1dc0eb1ef9286780ccc5fed632dfda0c02a3
Dave <99835156+davidhuckfield@users.noreply.github.com>
2025-03-27 18:45:25 +0000

    Update initialise_names.py

--- 7 kyu/initialise_names.py
+++ 7 kyu/initialise_names.py
@@ -1,2 +1,10 @@
 # link : https://www.codewars.com/kata/5768a693a3205e1cc100071f/train/python
 
+def initialize_names(name):
+    names = name.split()
+    print(names)
+    for i in range(len(names)):
+        if i != 0 and i != len(names)-1:
+            names[i] = names[i][0] + "."
+            
+    return " ".join(names)
