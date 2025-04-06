dce9ad3e53f4e3af56ef9e2ddbb7271927ae3fe5
davidhuckfield <djhuckfield@hotmail.co.uk>
2025-04-06 17:53:36 +0100

    Completed 6 kyu Python kata to round to nearest 0.5

---
+++ 6 kyu/round_to_nearest05.py
@@ -0,0 +1,10 @@
+# link : http://codewars.com/kata/51f1342c76b586046800002a/train/python
+
+import math
+
+def solution(n):
+    #multiply by 2 to shift the scale 
+    #add 0.5 before flooring to force tie-breaks upwards
+    #floor the result
+    #divide by 2 to shift the scale back
+    return math.floor(n * 2 + 0.5) / 2
