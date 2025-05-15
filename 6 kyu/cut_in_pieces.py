e84a5659e42c6307e10544b1706e03326eb7010a
David Huckfield <djhuckfield@hotmail.co.uk>
2025-05-15 13:07:18 +0100

    Started 6 kyu Python kata to return number of partitions adding up to input number

---
+++ 6 kyu/cut_in_pieces.py
@@ -0,0 +1,9 @@
+# link : https://www.codewars.com/kata/55fbb7063097cf0b6b000032/train/python
+
+def part_const(n, k, num):
+    pass
+
+print(part_const(10, 3, 2))
+print(part_const(10, 3, 0))
+print(part_const(10, 4, 1))
+print(part_const(10, 5, 3))
