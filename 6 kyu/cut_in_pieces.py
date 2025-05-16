debb0dd5c466fe10f6a5e11fa3cea6dbf76f3f13
David Huckfield <djhuckfield@hotmail.co.uk>
2025-05-16 08:27:06 +0100

    Completed 6 kyu Python kata to find how many partitions of length k of number n that don't include specified number

--- 6 kyu/cut_in_pieces.py
+++ 6 kyu/cut_in_pieces.py
@@ -1,7 +1,25 @@
 # link : https://www.codewars.com/kata/55fbb7063097cf0b6b000032/train/python
 
 def part_const(n, k, num):
-    pass
+    def partitions(n, k, max_val=None):
+        if max_val is None:
+            max_val = n
+        
+        if k == 1:
+            if 1 <= n <= max_val and n != num:
+                return 1
+            else:
+                return 0
+            
+        total = 0    
+        
+        for i in range(1, min(n, max_val) + 1):
+            if i == num:
+                continue
+            total += partitions(n - i, k - 1, i)
+        return total
+    
+    return partitions(n, k)
 
 print(part_const(10, 3, 2))
 print(part_const(10, 3, 0))
