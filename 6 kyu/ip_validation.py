f4d9f34689b50fd8c3da02b6a554d3c02bd201a3
David Huckfield <djhuckfield@hotmail.co.uk>
2025-05-30 08:15:12 +0100

    Alternative method for previously completed IP validation kata

--- 6 kyu/ip_validation.py
+++ 6 kyu/ip_validation.py
@@ -17,3 +17,14 @@ def is_valid_IP(strng):
         except:
             return False
     return True
+
+#alternative method using socket library
+
+import socket
+
+def is_valid_IP(addr):
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+        return True
+    except socket.error:
+        return False
