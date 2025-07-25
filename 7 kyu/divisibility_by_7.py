bf285c14013d8a2b008abbf1a44a6cd72fd1dfa8
David Huckfield <djhuckfield@hotmail.co.uk>
2025-07-25 09:04:23 +0100

    Annotated previously completed kata

--- 7 kyu/divisibility_by_7.py
+++ 7 kyu/divisibility_by_7.py
@@ -2,11 +2,11 @@
 
 def seven(m):
     number = m
-    counter = 0
-    while len(str(number)) > 2:
-        number_string = str(number)
-        last_digit = int(number_string[len(number_string)-1])
-        remaining_number = int(number_string[:len(number_string)-1])
-        number = (remaining_number - (last_digit * 2))
-        counter += 1
-    return (number, counter)
+    counter = 0 #counts how many steps required to reach number with no more than 2 digits
+    while len(str(number)) > 2: #reduce number until it's length 2 or lower
+        number_string = str(number) #convert number to string
+        last_digit = int(number_string[len(number_string)-1]) #take last digit from number string and convert back to int
+        remaining_number = int(number_string[:len(number_string)-1]) #slice number apart from last digit/character and convert back to int
+        number = (remaining_number - (last_digit * 2)) #deduct twice the last digit from remaining_number
+        counter += 1 #increment counter
+    return (number, counter) #return resulting number and how many steps to reach it
