be03c81db9f63c0c66d8473566ae3a9e543ea2c0
David Huckfield <djhuckfield@hotmail.co.uk>
2025-08-07 12:12:48 +0100

    Started 7 kyu Python kata to validate EANs

---
+++ 7 kyu/EAN_validation.py
@@ -0,0 +1,25 @@
+# link : https://www.codewars.com/kata/55563df50dda59adf900004d/train/python
+
+def validate_ean(code):
+    if len(code) != 13:
+        return False
+    sum = 0
+    for i in range(len(code)):
+        if i%2 == 0:
+            sum += int(code[i])
+        else:
+            sum += (int(code[i])*3)
+    print(sum)
+    if sum % 10 == 0:
+        checksum = 0
+    else:
+        checksum = (10 - (sum % 10))
+    print(checksum)
+    if checksum == code[12]:
+        return True
+    else:
+        return False
+
+print(validate_ean("9783815820865")) #True
+print(validate_ean("9783815820864")) #False
+print(validate_ean("9783827317100")) #True
