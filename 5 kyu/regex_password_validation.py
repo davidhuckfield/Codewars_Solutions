e7b3ec08b46edfc07129d398cd7dbdbec9c5983f
David Huckfield <djhuckfield@hotmail.co.uk>
2025-09-05 09:26:54 +0100

    Further solution for 5 kyu Python regex kata

--- 5 kyu/regex_password_validation.py
+++ 5 kyu/regex_password_validation.py
@@ -1,3 +1,15 @@
 # link : https://www.codewars.com/kata/52e1476c8147a7547a000811/train/python
 
 regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z0-9]{6,}$"
+
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
