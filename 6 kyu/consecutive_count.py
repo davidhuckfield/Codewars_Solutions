f6f8c8d77d17c1e9c51a0af6d86641f9240ec22d
David Huckfield <djhuckfield@hotmail.co.uk>
2025-09-11 08:52:01 +0100

    Completed 6 kyu Python kata to find longest consecutive series of specified character

--- 6 kyu/consecutive_count.py
+++ 6 kyu/consecutive_count.py
@@ -2,14 +2,18 @@
 
 def get_consective_items(items, key): 
     counter = 0
+    max_counter = 0
     items = list(str(items))
+    key = str(key)
     for item in items:
         if item == key:
             counter += 1
         else:
+            max_counter = max(max_counter, counter)
             counter = 0
+    max_counter = max(max_counter, counter)
     
-    return counter
+    return max_counter
 
 print(get_consective_items(90000, 0)) #4
 print(get_consective_items('ascasdaiiiasdacasdiiiiicasdasdiiiiiiiiiiisdasdasdiii', 'i')) #11
