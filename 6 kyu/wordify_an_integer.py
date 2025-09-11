55809322f5426abdb64234c6f4fa95d116e6ce69
David Huckfield <djhuckfield@hotmail.co.uk>
2025-09-11 08:54:48 +0100

    Setting up file and test cases for 6 kyu Python kata to convert numbers to words

---
+++ 6 kyu/wordify_an_integer.py
@@ -0,0 +1,11 @@
+# link : https://www.codewars.com/kata/553a2461098c64ae53000041/train/python
+
+def wordify(n):
+    pass
+
+print(wordify(1))
+print(wordify(10))
+print(wordify(25))
+print(wordify(103))
+print(wordify(347))
+print(wordify(999))
