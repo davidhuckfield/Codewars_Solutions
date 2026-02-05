b6008182ff52fd07e7cccbc7425b4ef08b8c6fe1
David Huckfield <djhuckfield@hotmail.co.uk>
2026-02-05 08:47:42 +0000

    Removed print statement and tested with larger number strings on previous kata

--- 6 kyu/non_even_substrings.py
+++ 6 kyu/non_even_substrings.py
@@ -2,7 +2,7 @@
 
 def solve(s):
     substrings = [s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)]
-    print(substrings)
+    #print(substrings)
     odds = [n for n in substrings if int(n) % 2 !=0]
     odds = [int(n) for n in odds]
     odds = sorted(odds)
@@ -11,3 +11,4 @@ def solve(s):
 
 
 print(solve("1341")) #7
+print(solve("12345678987654321"))
