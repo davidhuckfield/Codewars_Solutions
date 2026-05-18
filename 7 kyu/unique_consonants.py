19b45d5ea85879b84d221eade8e67ff21c989322
David Huckfield <djhuckfield@hotmail.co.uk>
2026-05-18 18:11:30 +0100

    Added file for new Python kata

---
+++ 7 kyu/unique_consonants.py
@@ -0,0 +1,7 @@
+# link : https://www.codewars.com/kata/5a19226646d843de9000007d/train/python
+
+def count_consonants(text):
+    #split text into list (also removes duplicates?)
+    #filter out vowels
+    #return length of list
+    pass
