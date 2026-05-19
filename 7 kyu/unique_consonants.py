673200b0e41de361f0b2d6ac54706bf5ed64b59f
David Huckfield <djhuckfield@hotmail.co.uk>
2026-05-19 17:55:41 +0100

    Completed 7 kyu Python kata to return number of unique consonants

--- 7 kyu/unique_consonants.py
+++ 7 kyu/unique_consonants.py
@@ -2,6 +2,15 @@
 
 def count_consonants(text):
     #split text into list (also removes duplicates?)
+    text_list = list(set(text.lower()))
+    vowels = ["a", "e", "i", "o", "u"]
+    print(text_list)
     #filter out vowels
-    #return length of list
-    pass
+    filtered_list = [char for char in text_list if char.isalpha() and char not in vowels ]
+    print(filtered_list)
+    print(len(filtered_list))
+    return len(filtered_list)
+
+count_consonants("sillystring")
+count_consonants("aeiou")
+count_consonants("Mom")
