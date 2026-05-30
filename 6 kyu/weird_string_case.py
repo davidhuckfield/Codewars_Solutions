9180254a2ef735e3413800f75b87ee3aa32d84c8
davidhuckfield <djhuckfield@hotmail.co.uk>
2026-05-30 22:48:39 +0100

    Completed 6 kyu Python kata to change case of letters in words based on their index

--- 6 kyu/weird_string_case.py
+++ 6 kyu/weird_string_case.py
@@ -1,7 +1,19 @@
 # link : https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/python
 
 def to_weird_case(words):
-    pass
+    words_split = words.split()
+    new_words = []
+    for word in words_split:
+        new_word = ""
+        for index, letter in enumerate(word):
+            if index % 2 == 0:
+                new_word += letter.upper()
+            else:
+                new_word += letter.lower()
+        new_words.append(new_word)
+    
+    return " ".join(new_words)
 
-print(to_weird_case("This"))
-print(to_weird_case("Is"))
+print(to_weird_case("This")) #ThIs
+print(to_weird_case("Is")) #Is
+print(to_weird_case("This is a Test")) #ThIs Is A TeSt
