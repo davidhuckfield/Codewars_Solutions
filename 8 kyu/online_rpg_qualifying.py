0bffc96a6c2a23673fd4cb7c220bfd001c4fe1b0
davidhuckfield <djhuckfield@hotmail.co.uk>
2026-06-07 19:54:15 +0100

    Completed 8 kyu Python kata

---
+++ 8 kyu/online_rpg_qualifying.py
@@ -0,0 +1,7 @@
+# link : https://www.codewars.com/kata/55849d76acd73f6cc4000087/train/python
+
+def player_rank_up(pts):
+    if pts >= 100:
+        return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+    else:
+        return False
