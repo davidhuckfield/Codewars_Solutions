d2871672956f178ce3df03db2a32258a3e87449c
David Huckfield <djhuckfield@hotmail.co.uk>
2026-06-19 15:34:01 +0100

    Set up file for Python kata

---
+++ 7 kyu/responsible_drinking.py
@@ -0,0 +1,7 @@
+# link : https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/python
+
+def hydrate(drink_string): 
+    pass
+
+print(hydrate("1 beer")) # "1 glass of water"
+print(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer")) #"10 glasses of water"
