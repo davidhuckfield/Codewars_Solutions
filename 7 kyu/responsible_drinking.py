096f8d6bd714db07b96f750137a5beeb833f3663
David Huckfield <djhuckfield@hotmail.co.uk>
2026-06-19 15:42:52 +0100

    Completed 7 kyu Python kata on interpreting numbers within a string

--- 7 kyu/responsible_drinking.py
+++ 7 kyu/responsible_drinking.py
@@ -1,7 +1,16 @@
 # link : https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/python
 
+import re
+
 def hydrate(drink_string): 
-    pass
+    numbers = re.findall(r'\d+', drink_string)
+    numbers = [int(number) for number in numbers]
+
+    total = sum(numbers)
+
+    return_string = f"{total} glass{'es' if total > 1 else ''} of water"
+
+    return return_string
 
 print(hydrate("1 beer")) # "1 glass of water"
 print(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer")) #"10 glasses of water"
